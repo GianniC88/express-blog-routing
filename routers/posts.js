61a902e83cb9345ec31ab6e957663ffe1a45af9d
GianniC88 <gianni.colucci.88@gmail.com>
2025-07-01 17:10:41 +0200

    Aggiungi metodi per la gestione dei post: store, update e destroy

--- routers/posts.js
+++ routers/posts.js
@@ -12,5 +12,21 @@ router.get('/:id', (req, res) => {
 	res.json('posts detail')
 });
 
-//
+//store
+router.post('/', (req, res) => {
+	res.json('Creazione un nuovo posts');
+});
+
+//update
+router.patch('/:id', (req, res) => {
+	res.json('modifica integrale del post')
+})
+
+
+//destroy
+router.delete('/:id', (req, res) => {
+	res.json('eliminazione post')
+})
+
+
 module.exports = router
